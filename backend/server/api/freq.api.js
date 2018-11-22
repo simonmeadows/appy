636ff9ad945155287834b5ead796a354199d18d7
simonmeadows <it@warehousesound.co.uk>
2018-11-22 11:51:18 +0000

    connected endpoint to mongo

--- backend/server/api/freq.api.js
+++ backend/server/api/freq.api.js
@@ -4,10 +4,47 @@ const Joi = require('joi')
 const Chalk = require('chalk')
 const errorHelper = require('../utilities/error-helper')
 
-const Config = require('../../config')
+// const Config = require('../../config')
 
 module.exports = function(server, mongoose, logger) {
-  // Contact Endpoint
+  // FreqSweep Get Sweep
+  ;(function() {
+    const Log = logger.bind(Chalk.magenta('FreqSweep'))
+
+    Log.note('Generating FreqSweep Endpoint')
+
+    const freqSweepGetHandler = async function(request, h) {
+      try {
+        const FreqScan = mongoose.model('freqscan')
+
+        return FreqScan.find()
+      } catch (err) {
+        errorHelper.handleError(err, Log)
+      }
+    }
+
+    server.route({
+      method: 'GET',
+      path: '/freqsweep',
+      config: {
+        handler: freqSweepGetHandler,
+        description: 'Get all a frequency sweeps',
+        tags: ['api', 'FreqSweep'],
+        validate: {},
+        plugins: {
+          'hapi-swagger': {
+            responseMessages: [
+              { code: 200, message: 'Success' },
+              { code: 400, message: 'Bad Request' },
+              { code: 404, message: 'Not Found' },
+              { code: 500, message: 'Internal Server Error' }
+            ]
+          }
+        }
+      }
+    })
+  })()
+  // FreqSweep POST New sweep
   ;(function() {
     const Log = logger.bind(Chalk.magenta('FreqSweep'))
 
@@ -15,20 +52,11 @@ module.exports = function(server, mongoose, logger) {
 
     const freqSweepHandler = async function(request, h) {
       try {
-        const mailer = request.server.plugins.mailer
-        const emailOptions = {
-          subject: Config.get('/projectName') + ' contact form',
-          to: Config.get('/system/toAddress'),
-          replyTo: {
-            name: request.payload.name,
-            address: request.payload.email
-          }
-        }
-        const template = 'contact'
+        const FreqScan = mongoose.model('freqscan')
+        let thisFreqScan = new FreqScan(request.payload)
+        await thisFreqScan.save()
 
-        await mailer.sendEmail(emailOptions, template, request.payload, Log)
-
-        return { message: 'Success' }
+        return FreqScan.find()
       } catch (err) {
         errorHelper.handleError(err, Log)
       }
@@ -48,6 +76,48 @@ module.exports = function(server, mongoose, logger) {
             ScanTime: Joi.date(),
             ScanData: Joi.array().items(Joi.array().items(Joi.number()))
           }
+        },
+        plugins: {
+          'hapi-swagger': {
+            responseMessages: [
+              { code: 200, message: 'Success' },
+              { code: 400, message: 'Bad Request' },
+              { code: 404, message: 'Not Found' },
+              { code: 500, message: 'Internal Server Error' }
+            ]
+          }
+        }
+      }
+    })
+  })()
+  // FreqSweep Delete sweep
+  ;(function() {
+    const Log = logger.bind(Chalk.magenta('FreqSweep'))
+
+    Log.note('Generating FreqSweep Endpoint')
+
+    const freqSweepRemoveAllHandler = async function(request, h) {
+      try {
+        const FreqScan = mongoose.model('freqscan')
+        if (request.payload.id === 'all') {
+          await FreqScan.remove(function() {})
+        }
+
+        return FreqScan.find()
+      } catch (err) {
+        errorHelper.handleError(err, Log)
+      }
+    }
+
+    server.route({
+      method: 'DELETE',
+      path: '/freqsweep',
+      config: {
+        handler: freqSweepRemoveAllHandler,
+        description: 'Remove all frequency sweeps',
+        tags: ['api', 'FreqSweep'],
+        validate: {
+          payload: { id: Joi.string().allow(null) }
         },
         plugins: {
           'hapi-swagger': {
---
+++ backend/server/models/freqscan.model.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// const RestHapi = require('rest-hapi')
+// const errorHelper = require('../utilities/error-helper')
+
+module.exports = function(mongoose) {
+  var modelName = 'freqscan'
+  // var Types = mongoose.Schema.Types
+  var Schema = new mongoose.Schema({
+    StartMHZ: Number,
+    StopMHZ: Number,
+    ScanTime: Date,
+    ScanData: [[Number, Number]]
+  })
+  Schema.statics = {
+    collectionName: modelName,
+    routeOptions: {
+      policies: {},
+      associations: {},
+      permissions: {}
+    }
+  }
+
+  return Schema
+}
